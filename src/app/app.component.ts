d103637a2d7d8c503c2f27c4e2a2f2cd9049a1e2
prashun <prashunpranjal21@gmail.com>
2017-07-13 22:46:00 +0530

    final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+title = "kk";
+stringGiven = "";
+
+
+isPalindrome=function (stringGiven:string): boolean{
+if (stringGiven== undefined)
+return false;
+  const reversed= stringGiven.split("").reverse().join("");
+  return reversed === stringGiven;
+}
+//if (stringGiven== undefined)
+//return false;
+    //if(stringGiven ==  stringGiven.split("").reverse().join(""))
+    //return true;
+    //else return false;}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,8 +8,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule      
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
